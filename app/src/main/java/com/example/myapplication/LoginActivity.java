66bd8fa2f8f3105b42dd06501ab62923a3a11787
VarGun <heegun4690@naver.com>
2023-03-14 20:42:52 +0900

    feat: getIdToken

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,25 +2,32 @@ package com.example.myapplication;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
 
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+// Todo : Google Login access token 처리 + 일기 비밀번호 설정 dialog + profile img -> google 계정 사진 + 무한스크롤 ..
 
 public class LoginActivity extends AppCompatActivity {
     TextView txt_login;
@@ -41,8 +48,11 @@ public class LoginActivity extends AppCompatActivity {
         isLogin = false;
         txt_login = findViewById(R.id.txt_login);
         btn_google = findViewById(R.id.btn_google);
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
+
+
+        mGoogleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+
 
         txt_login.setTypeface(txt_login.getTypeface(), Typeface.ITALIC);
 
@@ -51,57 +61,53 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 isLogin = true;
-                signIn();
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
-
     }
 
 
-    public void updateUI(GoogleSignInAccount account){
-        if(account != null){
-            String personName = account.getDisplayName();
-            String personGivenName = account.getGivenName();
-            String personFamilyName = account.getFamilyName();
-            String personEmail = account.getEmail();
-            String personId = account.getId();
-            Uri personPhoto = account.getPhotoUrl();
-            Toast.makeText(this, "personName : "+ personName, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "personGivenName : "+ personGivenName, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "personFamilyName : "+ personFamilyName, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "personEmail : "+ personEmail, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "personId : "+ personId, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "personPhoto : "+ personPhoto, Toast.LENGTH_SHORT).show();
-            startActivity( new Intent(this, MainActivity.class));
-            finish();
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            // 사용자가 로그인한 경우
+            String idToken = account.getIdToken(); // 로그인 토큰을 가져옵니다.
+            String name = account.getGivenName();
+            String ac = account.getServerAuthCode();
+            // TODO: 이제이 토큰을 사용하여 서버에서 사용자를 인증하십시오.
+            Toast.makeText(this, "idToken : " + idToken, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "name : " + name, Toast.LENGTH_SHORT).show();
+
+        } else {
+            // 사용자가 로그인하지 않은 경우
+            // TODO: 사용자에게 다시 로그인하도록 요청하십시오.
         }
-        else{
-            Toast.makeText(this, "You Didn't Sing in", Toast.LENGTH_SHORT).show();
-
-        }
-
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == RC_SIGN_IN){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Toast.makeText(this, "onActivityResult success ", Toast.LENGTH_SHORT).show();
             handleSignInResult(task);
         }
     }
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask){
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(this, "handleSignInResult success ", Toast.LENGTH_SHORT).show();
+
+            // Signed in successfully, show authenticated UI.
             updateUI(account);
-        }catch (ApiException e){
-            Log.w(TAG,"signInResult:failed code = " + e.getStatusCode());
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(this, "handleSignInResult fail " + "signInResult:failed code=" + e.getMessage(), Toast.LENGTH_SHORT).show();
             updateUI(null);
         }
     }
+
 }
