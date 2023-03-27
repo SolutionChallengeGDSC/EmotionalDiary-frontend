c44c5ebd80907664c477701843c300a871da7d63
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2023-03-28 03:22:05 +0900

    feat: 일기 작성 페이지 닫기 버튼 구현

--- app/src/main/java/com/GDSC/emotionaldiary/CreateDiaryActivity.java
+++ app/src/main/java/com/GDSC/emotionaldiary/CreateDiaryActivity.java
@@ -1,41 +1,28 @@
 package com.GDSC.emotionaldiary;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-// Todo 감정 점수 세팅 ..
-public class CreateDiaryActivity extends AppCompatActivity {
-    private ImageButton emotional_score_btn, lock, close_btn;
-    EditText title, content;
-    AppCompatButton complete_btn;
-    private static final int RESULT_CREATEDIARY = 1;
 
+public class CreateDiaryActivity extends AppCompatActivity {
+    private ImageButton close_btn, emotional_score_btn, lock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_diary);
-        Intent getDetailIntent = getIntent();
-        title = findViewById(R.id.title);
-        content = findViewById(R.id.content);
+
+        // 닫기 버튼
         close_btn = findViewById(R.id.close_btn);
-        complete_btn = findViewById(R.id.complete_btn);
-        try{
-            title.setText(getDetailIntent.getStringExtra("title"));
-            content.setText(getDetailIntent.getStringExtra("content"));
-        }catch (NullPointerException e){
-            title.setText("");
-            content.setText("");
-        }
+        close_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         // 감정 점수 다이얼로그 띄우기
         emotional_score_btn = findViewById(R.id.emotional_score_btn);
         emotional_score_btn.setOnClickListener(new View.OnClickListener() {
@@ -44,24 +31,6 @@ public class CreateDiaryActivity extends AppCompatActivity {
                 EmotionalScoreDialog emotionalScoreDialog = new EmotionalScoreDialog();
                 emotionalScoreDialog.show(getSupportFragmentManager(), "emotional score");
 
-            }
-        });
-        close_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        complete_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreateDiaryActivity.this, DetailDiaryActivity.class);
-                intent.putExtra("title", title.getText().toString());
-                intent.putExtra("content", content.getText().toString());
-                Toast.makeText(CreateDiaryActivity.this, "complete_btn Clicked", Toast.LENGTH_SHORT).show();
-                Log.e("content", content.getText().toString());
-                setResult(RESULT_CREATEDIARY, intent);
-                finish();
             }
         });
 
@@ -78,9 +47,5 @@ public class CreateDiaryActivity extends AppCompatActivity {
                 }
             }
         });
-
-        // 액션바 제거
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.hide();
     }
 }
